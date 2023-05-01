9bc258527fc8b9b60aaf244c00ab8b7940aa2644
Gustavo Gaspar <glgaspar@live.com>
2023-05-01 18:17:00 -0300

    getting console out of the way while capturing

--- Program.cs
+++ Program.cs
@@ -2,28 +2,40 @@
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace screenRecorder
 {
     internal class Program
     {
+        // import some dll stuff just so I can minimize the console for the capture
+        // kinda useless if it screenshots and half the screen is itself
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
         static void Main(string[] args)
         {
             while (true)
             {
                 Console.WriteLine("Select option \n0 - to cancel\n1 - for screenshot\n2 - For recording");
                 string option = Console.ReadLine();
+                IntPtr hWnd = GetConsoleWindow();
 
 
                 if (option == "1")
                 {
+                    ShowWindow(hWnd, 0);
                     Shoot();
+                    ShowWindow(hWnd, 1);
                     Console.WriteLine("OK, done. Bye...");
                     break;
                 }
                 else if (option == "2")
                 {
+                    ShowWindow(hWnd, 0);
                     Rec();
+                    ShowWindow(hWnd, 1);
                     Console.WriteLine("OK, done. Bye...");
                     break;
                 }
@@ -62,7 +74,7 @@ namespace screenRecorder
             Graphics graphic = Graphics.FromImage(captureBitmap);
 
             graphic.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
-            captureBitmap.Save($"{path}/screenShooter.jpg", ImageFormat.Jpeg);
+            captureBitmap.Save($"{path}/screenShotter.jpg", ImageFormat.Jpeg);
         }
     }
 }
