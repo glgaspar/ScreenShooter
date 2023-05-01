5ae0e6b6e51030d786a9a5f26d1b7c380ca21d4f
Gustavo Gaspar <glgaspar@live.com>
2023-05-01 15:44:42 -0300

    screen shots done. Still gotta figura out the recording part

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+
+namespace screenRecorder
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            Console.WriteLine(path);
+
+            // getting screen dimensions
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+            // creating bitmap obj
+            Bitmap captureBitmap = new Bitmap(screenWidth, screenHeight, PixelFormat.Format64bppArgb);
+            
+
+            Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+
+            Graphics graphic = Graphics.FromImage(captureBitmap);
+
+            graphic.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+            captureBitmap.Save($"{path}/screenShotter.jpg", ImageFormat.Jpeg);
+
         }
     }
 }
