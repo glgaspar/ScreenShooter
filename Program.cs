ff654ea2be9afc016253b3fe469fb9161605abe0
Gustavo Gaspar <glgaspar@live.com>
2023-05-01 18:15:07 -0300

    adding  logic to choose screen shot or record

--- Program.cs
+++ Program.cs
@@ -3,15 +3,51 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
 
-
 namespace screenRecorder
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Select option \n0 - to cancel\n1 - for screenshot\n2 - For recording");
+                string option = Console.ReadLine();
+
+
+                if (option == "1")
+                {
+                    Shoot();
+                    Console.WriteLine("OK, done. Bye...");
+                    break;
+                }
+                else if (option == "2")
+                {
+                    Rec();
+                    Console.WriteLine("OK, done. Bye...");
+                    break;
+                }
+                else if (option == "0")
+                {
+                    Console.WriteLine("Ending program...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please select a valid option");
+                };
+            }
+
+        }
+
+        static void Rec()
+        {
+
+        }
+
+        static void Shoot()
+        {
             string path = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            Console.WriteLine(path);
 
             // getting screen dimensions
             int screenWidth = Screen.PrimaryScreen.Bounds.Width;
@@ -19,15 +55,14 @@ namespace screenRecorder
 
             // creating bitmap obj
             Bitmap captureBitmap = new Bitmap(screenWidth, screenHeight, PixelFormat.Format64bppArgb);
-            
+
 
             Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
 
             Graphics graphic = Graphics.FromImage(captureBitmap);
 
             graphic.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
-            captureBitmap.Save($"{path}/screenShotter.jpg", ImageFormat.Jpeg);
-
+            captureBitmap.Save($"{path}/screenShooter.jpg", ImageFormat.Jpeg);
         }
     }
 }
